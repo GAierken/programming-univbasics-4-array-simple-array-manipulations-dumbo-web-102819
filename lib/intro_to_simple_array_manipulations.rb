90d5a3b508d26d0c474ed42da0c4d761e2ff8540
Guligena Aierken <guligena_aierken@163.com>
2019-10-17 21:08:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,7 +10,8 @@ end
 
 def using_pop (array)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop
+  deleted_string=continents.pop
+  p deleted_string
 end
 
 def pop_with_args(array)
