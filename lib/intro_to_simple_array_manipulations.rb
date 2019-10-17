9cf81268abc0f8ce3183dbbe243c12329e0f8692
Guligena Aierken <guligena_aierken@163.com>
2019-10-17 21:16:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,11 +14,10 @@ end
 
 def pop_with_args(array)
   array.pop(2)
-end 
+end
 
 def using_shift(array)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  my_favorite_cities.shift
+  array.shift
 end
 
 def shift_with_args (array)
